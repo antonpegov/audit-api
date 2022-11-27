58a030ea66f86e53ca01d75edb928b11b149de6d
Anton Pegov <antonpegov@gmail.com>
2022-11-27 18:38:55 +0100

    [init] Add get auditors endpoint

--- apps/auditors/src/auditors.controller.ts
+++ apps/auditors/src/auditors.controller.ts
@@ -1,26 +1,30 @@
-import { Controller, Post, Body } from '@nestjs/common'
+import { Controller, Post, Body, Logger, Get } from '@nestjs/common'
 import { ApiCreatedResponse } from '@nestjs/swagger'
 
+import { Auditor } from '@auditors/schemas/auditor.schema'
 import { AuditorsService } from '@auditors/auditors.service'
 import { CreateAuditorRequest } from '@auditors/dto/create-auditor.dto'
-import { Auditor } from './schemas/auditor.schema'
 
 @Controller('auditors')
 export class AuditorsController {
   constructor(private readonly auditorsService: AuditorsService) {}
 
   @Post()
-  createAuditor(@Body() request: CreateAuditorRequest) {
-    return this.auditorsService.createAuditor(request)
-  }
-
-  @Post()
   @ApiCreatedResponse({
     description: 'The record has been successfully created.',
     type: Auditor,
   })
-  async create(@Body() request: CreateAuditorRequest): Promise<Auditor> {
+  create(@Body() request: CreateAuditorRequest) {
     return this.auditorsService.createAuditor(request)
+  }
+
+  @Get()
+  @ApiCreatedResponse({
+    description: 'The records a successfully recieved.',
+    type: Auditor,
+  })
+  find() {
+    return this.auditorsService.getAuditors()
   }
 }
 
--- apps/auditors/src/auditors.service.ts
+++ apps/auditors/src/auditors.service.ts
@@ -1,14 +1,27 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, Logger } from '@nestjs/common'
 
 import { AuditorsRepository } from '@auditors/auditors.repository'
 import { CreateAuditorRequest } from '@auditors/dto/create-auditor.dto'
+import { Auditor } from './schemas/auditor.schema'
 
 @Injectable()
 export class AuditorsService {
   constructor(private readonly auditorsRepository: AuditorsRepository) {}
 
-  createAuditor(request: CreateAuditorRequest) {
-    return this.auditorsRepository.create(request)
+  createAuditor(request: CreateAuditorRequest): Promise<Omit<Auditor, 'password'>> {
+    return this.auditorsRepository.create(request).then((auditor) => {
+      delete auditor.password
+
+      return auditor
+    })
+  }
+
+  getAuditors(): Promise<Auditor[]> {
+    return this.auditorsRepository.find({}).then((auditors) => {
+      auditors.forEach((auditor) => delete auditor.password)
+
+      return auditors
+    })
   }
 }
 
