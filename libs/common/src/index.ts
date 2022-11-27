01ba21546e575c3a5e232d202304d14ffeff6a2e
Anton Pegov <antonpegov@gmail.com>
2022-11-27 07:47:58 +0100

    [init] Add abstract database repository

--- libs/common/src/index.ts
+++ libs/common/src/index.ts
@@ -1,2 +1,4 @@
-export * from './common.module';
-export * from './common.service';
+export * from './database/abstract.schema'
+export * from './database/abstract repository'
+export * from './database/database.module'
+
